86ddf2aa6067c14e266bf378f3cc9786e3271319
L200180164 <44195765+L200180164@users.noreply.github.com>
2018-12-23 23:49:54 +0700

    Add files via upload

---
+++ Praktikumm12/cgi-bin/index.py
@@ -0,0 +1,41 @@
+print """<DOCTYPE html>
+<html>
+	<head>
+		<title>Kegiatan 2</title>
+	</head>
+	<body>
+		<table border='0'>
+			<tr>
+				<td colspan='4'><p align='center'><b>Data Diri</b></p></td>
+			</tr>
+			<tr>
+				<td rowspan='6'><img src='../../aku.jpeg' width='200' height='250'></td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td>:</td>
+				<td>Hafid Yudo H</td>
+			</tr>
+			<tr>
+				<td>Alamat Tinggal</td>
+				<td>:</td>
+				<td>Jl.Semangka </td>
+			</tr>
+			<tr>
+				<td>Tempat, tanggal lahir</td>
+				<td>:</td>
+				<td>Jakarta,26 Januari 2000</td>
+			</tr>
+			<tr>
+				<td>Tempat wisata favorit</td>
+				<td>:</td>
+				<td>Air Terjun</td>
+			</tr>
+			<tr>
+				<td>Motto</td>
+				<td>:</td>
+				<td>Kerjakan Semua yang Halal</td>
+			</tr>
+			
+	</body>
+</html>"""
